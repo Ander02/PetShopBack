148f09a657d178ed4f7037869c9cfa9ea2f994d3
Ander02 <anderson.pessoa002@gmail.com>
2019-12-01 16:10:34 -0300

    List test

---
+++ Test/Unit/Features/Users/ListTest.cs
@@ -0,0 +1,47 @@
+﻿using Api.Features.Users;
+using FluentAssertions;
+using PetShop;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Test.Unit.Features.Users
+{
+    public class ListTest : BaseTestClass
+    {
+        public ListTest(ServerTestFixture<Startup> fixture) : base(fixture) { }
+
+        [Fact]
+        public async Task ListarUsuariosSemFiltroRetornaTodosOsUsuarios()
+        {
+            var result = await mediator.Send(new List.Query { });
+
+            result.Should().NotBeNull();
+            result.Should().HaveCount(1);
+        }
+
+        [Fact]
+        public async Task ListarUsuariosComFiltroFiltroRetornaUsuariosFiltrados()
+        {
+            var result = await mediator.Send(new List.Query
+            {
+                Search = "Admin"
+            });
+
+            result.Should().NotBeNull();
+            result.Should().HaveCount(1);
+        }
+
+        [Fact]
+        public async Task ListarUsuariosComFiltroFiltroRetornaUsuariosFiltrados2()
+        {
+            var result = await mediator.Send(new List.Query
+            {
+                Search = $"{Guid.NewGuid()}"
+            });
+
+            result.Should().NotBeNull();
+            result.Should().HaveCount(0);
+        }
+    }
+}
